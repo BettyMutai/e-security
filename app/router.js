b327ec89a796e484fdde34a0ee24e9c42d42c13d
Betty Mutai <bettymutai00@gmail.com>
2017-06-23 10:53:00 +0300

    Created routes and components for my app

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('cart');
+  this.route('contact');
+  this.route('product');
 });
 
 export default Router;
